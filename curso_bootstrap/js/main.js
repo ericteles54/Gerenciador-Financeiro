5dce30ff2d4084baa8a83b354b443682b5faf450
Eric Teles <ericteles54@hotmail.com>
2017-01-25 12:50:01 -0200

    curso do bootstrap na parte de javascripts

--- curso_bootstrap/js/main.js
+++ curso_bootstrap/js/main.js
@@ -1,5 +1,17 @@
 /* Scripts personalizados para o site */
 
+// Função para tratar e abrir o modal na abertura da página
 $(function(){
-    //alert("ola");
+        // equivalente ao load da pagina
+
+        $("#modal01").modal({backdrop:false, keyboard:false}); // Impede fechamento da janela com ESC
+
+        $("#modal01").modal("show");
+
+        setTimeout(function(){
+                $("#modal01").modal("hide");
+        }, 3000);
+
 });
+
+
