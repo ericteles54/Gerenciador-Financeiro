690c3bfa15d07acc8ea24f069a073a0ff0b0a38e
Eric - Note Home <ericteles54@hotmail.com>
2017-01-26 06:05:57 -0200

    iniciando projeto do curso bootstrap

--- curso_bootstrap/js/main.js
+++ curso_bootstrap/js/main.js
@@ -1,9 +1,9 @@
 /* Scripts personalizados para o site */
 
-// Função para tratar e abrir o modal na abertura da página
+// equivalente ao load da pagina
 $(function(){
-        // equivalente ao load da pagina
 
+        // Função para tratar e abrir o modal na abertura da página
         $("#modal01").modal({backdrop:false, keyboard:false}); // Impede fechamento da janela com ESC
 
         $("#modal01").modal("show");
@@ -11,6 +11,40 @@ $(function(){
         setTimeout(function(){
                 $("#modal01").modal("hide");
         }, 3000);
+
+
+
+
+        // Funcao para tratar o tooltip
+        $(".ttp").tooltip({
+                animation:false,
+                delay : { show : 500, hide : 5000 },
+                title : "Titulo padrao!",
+                trigger : 'click'
+        });
+
+        // Funcao para tratar o popover
+        $(".ppv").popover({
+                title : "Titulo javascript do popover",
+                trigger : 'hover focus'
+        });
+
+
+        // Funcao para tratar checkbox button com javascript
+        $(".btnjs").button({
+
+        });
+
+
+        // Funcao para tratar botao com troca de estado
+        $("#troca-estado").click(function(){
+                var btn = $(this);
+                btn.button("loading");
+
+                setTimeout(function(){
+                        btn.button("reset");
+                }, 3000);
+        });
 
 });
 
