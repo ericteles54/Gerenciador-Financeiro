f5956142ee3f966fec4985bcce44f711fb1dd8e8
Eric - Note Home <ericteles54@hotmail.com>
2017-01-27 05:53:34 -0200

    curso do bootstrap fazer aula 40

---
+++ curso_bootstrap/projeto_curso/js/rolagem_tela.js
@@ -0,0 +1,22 @@
+/* Scripts personalizados para o site */
+
+// Faz a rolagem suaveda pagina durante a navegacao dos links
+$(document).on('click', 'a[href^="#"]', function(e) {
+// target element id
+var id = $(this).attr('href');
+
+// target element
+var $id = $(id);
+if ($id.length === 0) {
+        return;
+}
+
+// prevent standard hash navigation (avoid blinking in IE)
+e.preventDefault();
+
+// top position relative to the document
+var pos = $(id).offset().top;
+
+// animated top scrolling
+$('body, html').animate({scrollTop: pos});
+});
